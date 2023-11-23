14af6dea2f3d452f6a3f94395030dc8a8334ec80
Kong To <newlight77@gmail.com>
2023-11-23 20:29:48 +0100

    feature(sonar-properties) : logging for troubleshooting on github actions

--- .github/actions/sonar-properties/src/main.ts
+++ .github/actions/sonar-properties/src/main.ts
@@ -1,22 +1,24 @@
 import * as core from '@actions/core';
+import * as path from 'path';
 import { ensureAllowedStack } from './properties/policies';
 import { loadProperties, mergeProperties, writeProperties } from './properties/prop-util';
 
-const templatesPath = '../../templates';
-
+let templatesPath = path.join(__dirname, '../../templates');
 
 export async function mergePropertiesFile(propPath: string, projectStack: string) {
     const stack = ensureAllowedStack(projectStack);
 
     const defaultProp = loadProperties(templatesPath, 'default.properties');
-    core.info(`default.properties : ${defaultProp}`);
+    core.info('default.properties', defaultProp.format());
     
     const projectProp = loadProperties(templatesPath, `${stack}.properties`);
-    core.info(`default.properties : ${projectProp}`);
+    core.info(`${stack}.properties`, projectProp.format());
 
     const mergedProp = mergeProperties(defaultProp, projectProp);
 
+    core.info('merged properties', mergedProp.format());
     writeProperties(propPath, 'sonar-project.properties', mergedProp)
+    core.info(`merged properties : ${propPath}/sonar-project.properties`);
 }
 
 function run() {
